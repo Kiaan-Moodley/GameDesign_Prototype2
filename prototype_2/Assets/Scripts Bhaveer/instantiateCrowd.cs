f758b734b066833c1142791d2102616c1bf4773d
BhaveerH <2116470@students.wits.ac.za>
2022-08-20 12:30:51 +0200

    added kneel mechanic

---
+++ prototype_2/Assets/Scripts Bhaveer/instantiateCrowd.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateCrowd : MonoBehaviour
+{
+    public GameObject goPrefab, charC;
+    public float startTime = 3f;
+    public float iCount = -5;
+    public float timer = 0f, timer2 = 0f;
+    public bool isTimer = false, isTimer2 = false;
+    public CharacterController ccPerson;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = startTime;
+        timer2 = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Instantiate(goPrefab, new Vector3(iCount, 5, -1.1f), Quaternion.identity);
+                Instantiate(goPrefab, new Vector3(iCount + 0.4f, 5, 0f), Quaternion.identity);
+                iCount += 0.8f;
+                timer2 = startTime;
+                isTimer2 = true;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ccPerson.height = 0;
+            timer = startTime;
+            isTimer = true;
+        }
+
+        if (isTimer == true)
+        {
+            timer -= 1 * Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                timer = 0;
+            }
+            
+            if (timer == 0)
+            {
+                ccPerson.height = 1.8f;
+                isTimer = false;
+            }
+        }
+
+        if (isTimer2 == true)
+        {
+            timer2 -= 1 * Time.deltaTime;
+
+            if (timer2 <= 0)
+            {
+                timer2 = 0;
+            }
+
+            if (timer2 == 0)
+            {
+                GameObject[] taggedgo = GameObject.FindGameObjectsWithTag("crowd");
+                foreach(GameObject @object in taggedgo)
+                {
+                    Destroy(@object);
+                }
+                isTimer2 = false;
+            }
+        }
+    }
+}
