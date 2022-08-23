33614ee4994d5259085c7cca844398bf24195683
DianeBada <92454613+DianeBada@users.noreply.github.com>
2022-08-23 18:36:01 +0200

    main menu

--- prototype_2/Assets/Scripts (Kiaan)/StalkeePOV.cs
+++ prototype_2/Assets/Scripts (Kiaan)/StalkeePOV.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
-using TMPro;
 
 
 public class StalkeePOV : MonoBehaviour
-{
+{   
     //Movement
     NavMeshAgent agent;
     public Transform[] waypoints;
@@ -24,23 +23,12 @@ public class StalkeePOV : MonoBehaviour
     bool isWalking;
     bool isTurning;
 
-    float minDist = 2.5f;
-    float maxDist = 20f;
-
+    float minDist = 5f;
+    float maxDist = 10f;
     float Dist;
 
     [SerializeField]
     private CurrentState _CurrentState;
-
-    public bool timerIsRunning = false;
-    public bool timerIsRunningClose = false;
-    public float timeRemaining = 10;
-    public float timeFarRemaining = 10;
-    float seconds;
-    float secondsFar;
-
-    bool tooClose = false;
-    bool tooFar = false;
     enum CurrentState
     {
         Idle,
@@ -48,21 +36,12 @@ public class StalkeePOV : MonoBehaviour
         Looking
 
     }
-
-    public GameObject TooFarAlert;
-    public GameObject TooCloseAlert;
-    public TextMeshProUGUI timerFar;
-    public TextMeshProUGUI timerClose;
-
     private void Start()
     {
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         UpdateDestination();
-        //_CurrentState = CurrentState.Idle;
-        tooClose = false;
-        tooFar = false;
-
+        _CurrentState = CurrentState.Idle;
 
     }
     private void AnimationChecker()
@@ -89,32 +68,28 @@ public class StalkeePOV : MonoBehaviour
 
     private void Update()
     {
-        timerClose.text = seconds.ToString();
-        timerFar.text = secondsFar.ToString();
         DistBetPlayers();
         AnimationChecker();
-        CheckTimer();
-        // if (isWalking)
-        // {
-        //     anim.Play("Walk");
-        // }
-        // else if (agent.transform.position == waypoints[i].position)
-        // {
-        //     anim.Play("Idle");
-        //     isTurning = true;
-        // }
+       // if (isWalking)
+       // {
+       //     anim.Play("Walk");
+       // }
+       // else if (agent.transform.position == waypoints[i].position)
+       // {
+       //     anim.Play("Idle");
+       //     isTurning = true;
+       // }
 
-        //else if (isTurning)
-        // {
-        //     anim.Play("Turn");
-        // }
+       //else if (isTurning)
+       // {
+       //     anim.Play("Turn");
+       // }
 
         //AI Movement
-        if (Vector3.Distance(transform.position, target) < 1)
+        if (Vector3.Distance(transform.position,target)<1)
         {
             IterateWayPoint();
             UpdateDestination();
-            Check();
             _CurrentState = CurrentState.Looking;
 
 
@@ -135,21 +110,19 @@ public class StalkeePOV : MonoBehaviour
             }
         }
 
-
+       
     }
 
     //Set Destination
     void UpdateDestination()
     {
         target = waypoints[i].position;
-        _CurrentState = CurrentState.Walking;
 
 
         StartCoroutine(Delay());
 
         IEnumerator Delay()
-        {
-
+        { 
             yield return new WaitForSeconds(5);
             _CurrentState = CurrentState.Walking;
 
@@ -161,7 +134,7 @@ public class StalkeePOV : MonoBehaviour
     {
         for (int i = 0; i < waypoints.Length; i++)
         {
-            if (agent.transform.position == waypoints[i].position)
+            if(agent.transform.position == waypoints[i].position)
             {
                 Debug.Log("just landed");
 
@@ -176,7 +149,7 @@ public class StalkeePOV : MonoBehaviour
     void IterateWayPoint()
     {
         i++;
-        if (i == waypoints.Length)
+        if(i == waypoints.Length)
         {
             isWalking = false;
             isIdle = true;
@@ -184,121 +157,26 @@ public class StalkeePOV : MonoBehaviour
             i = 0;  //restart AI Loop
         }
 
-
+        
 
     }
 
     public void DistBetPlayers()
     {
         Dist = Vector3.Distance(agent.transform.position, player.transform.position);
-        if (Dist < minDist)
+        if(Dist< minDist)
         {
             //timer starts
             Debug.Log("You are too close!");
-            tooClose = true;
-            Debug.Log("Tooclose is true");
-
-            timerIsRunningClose = true;
-            Debug.Log("Tooclose is true and time is running");
-
-            TooCloseAlert.SetActive(true);
-        }
-        else
-        {
-            tooClose = false;
-            TooCloseAlert.SetActive(false);
-            timerIsRunningClose = false;
-            timeRemaining = 10;
-
         }
 
-        if (Dist > maxDist)
+         if(Dist> maxDist)
         {
             //timer starts
             Debug.Log("You are too far!");
-            tooFar = true;
-            TooFarAlert.SetActive(true);
-            timerIsRunning = true;
-
-        }
-        else
-        {
-            tooFar = false;
-            TooFarAlert.SetActive(false);
-            timerIsRunning = false;
-            timeFarRemaining = 10;
-
 
         }
     }
 
-    void CheckTimer()
-    {
-        seconds = Mathf.FloorToInt(timeRemaining % 60);
-        secondsFar = Mathf.FloorToInt(timeFarRemaining % 60);
 
-
-
-
-        if (timerIsRunningClose && tooClose == true)
-        {
-            Debug.Log("Too close timr is running");
-            timerClose.text = seconds.ToString();
-            timerClose.gameObject.SetActive(true);
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-                timerClose.text = seconds.ToString();
-
-            }
-
-            else
-            {
-                timerClose.gameObject.SetActive(false);
-                //Game over
-                Debug.Log("Game over");
-                timeRemaining = 0;
-                timerIsRunningClose = false;
-            }
-
-
-            //if (tooClose == false)
-            //{
-            //    timeRemaining = 10;
-            //    timerIsRunning = false;
-
-            //}
-        }
-
-        else if (timerIsRunning && tooFar == true)
-        {
-            timerFar.text = seconds.ToString();
-            timerFar.gameObject.SetActive(true);
-            if (timeFarRemaining > 0)
-            {
-                timeFarRemaining -= Time.deltaTime;
-                timerFar.text = secondsFar.ToString();
-            }
-            else
-            {
-                timerFar.gameObject.SetActive(false);
-                //Game over
-                Debug.Log("Game over");
-                timeFarRemaining = 0;
-                timerIsRunning = false;
-            }
-            //if (tooFar == false)
-            //{
-            //    timeFarRemaining = 10;
-            //    timerIsRunning = false;
-
-            //}
-        }
-
-    }
 }
-
-
-
-
-
