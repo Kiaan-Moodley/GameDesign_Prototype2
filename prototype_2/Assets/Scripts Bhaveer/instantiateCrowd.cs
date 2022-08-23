6ee4380588c71db615121bd7ddbaf84dbe2d501a
BhaveerH <2116470@students.wits.ac.za>
2022-08-23 09:24:56 +0200

    added buttons for options

--- prototype_2/Assets/Scripts Bhaveer/instantiateCrowd.cs
+++ prototype_2/Assets/Scripts Bhaveer/instantiateCrowd.cs
@@ -35,7 +35,8 @@ public class instantiateCrowd : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            ccPerson.height = 0;
+             ccPerson.height = 0;
+           //ccPerson.transform.position = new Vector3(ccPerson.transform.position.x, 0.3f, ccPerson.transform.position.z);
             timer = startTime;
             isTimer = true;
         }
@@ -51,7 +52,8 @@ public class instantiateCrowd : MonoBehaviour
             
             if (timer == 0)
             {
-                ccPerson.height = 1.8f;
+                 ccPerson.height = 2f;
+               // ccPerson.transform.position = new Vector3(ccPerson.transform.position.x, 0.6444f, ccPerson.transform.position.z);
                 isTimer = false;
             }
         }
