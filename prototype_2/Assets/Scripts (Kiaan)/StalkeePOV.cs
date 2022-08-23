634718a98fcd1c72ab7148496a28867e450a3d69
Kiaan Moodley <2202459@students.wits.ac.za>
2022-08-23 09:42:56 +0200

    Update StalkeePOV.cs

--- prototype_2/Assets/Scripts (Kiaan)/StalkeePOV.cs
+++ prototype_2/Assets/Scripts (Kiaan)/StalkeePOV.cs
@@ -31,7 +31,6 @@ public class StalkeePOV : MonoBehaviour
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         UpdateDestination();
-
     }
 
     private void Update()
@@ -59,8 +58,6 @@ public class StalkeePOV : MonoBehaviour
             IterateWayPoint();
             UpdateDestination();
             Check();
-
-
         }
 
         //AI Sight and Detection
@@ -76,26 +73,20 @@ public class StalkeePOV : MonoBehaviour
                 }
             }
         }
-
-       
     }
 
     //Set Destination
     void UpdateDestination()
     {
-        StartCoroutine(Text());
 
+        target = waypoints[i].position;
+        StartCoroutine(Text());
         IEnumerator Text()
         {
-
             yield return new WaitForSeconds(5);
-
-            target = waypoints[i].position;
             isWalking = true;
             agent.SetDestination(target);
             isIdle = true;
-
-
         }
     }
 
@@ -118,16 +109,13 @@ public class StalkeePOV : MonoBehaviour
     void IterateWayPoint()
     {
         i++;
-        if(i == waypoints.Length)
+        if (i == waypoints.Length)
         {
             isWalking = false;
             isIdle = true;
             Debug.Log("End Point");
             i = 0;  //restart AI Loop
         }
-
-        
-
     }
 
     public void DistBetPlayers()
@@ -146,6 +134,4 @@ public class StalkeePOV : MonoBehaviour
 
         }
     }
-
-
 }
