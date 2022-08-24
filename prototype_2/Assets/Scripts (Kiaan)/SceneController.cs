3a4ca790e691e97e90acd1c323243dc172cc2ed5
DianeBada <92454613+DianeBada@users.noreply.github.com>
2022-08-24 08:39:27 +0200

    added a few more scene and scenecontroller script

--- prototype_2/Assets/Scripts (Kiaan)/SceneController.cs
+++ prototype_2/Assets/Scripts (Kiaan)/SceneController.cs
@@ -9,4 +9,36 @@ public class SceneController : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void CreditScene()
+    {
+        SceneManager.LoadScene("CreditScene");
+
+    }
+
+    public void RulesScene()
+    {
+        SceneManager.LoadScene("RulesScene");
+
+    }
+    public void IntroScene()
+    {
+        SceneManager.LoadScene("IntroScene");
+
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("GameScene");
+
+    }
+    public void WinScene()
+    {
+        SceneManager.LoadScene("WinScene");
+
+    }
+    public void LoseScene()
+    {
+        SceneManager.LoadScene("LoseScene");
+
+    }
 }
